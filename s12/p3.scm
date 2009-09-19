47df0a4e479482ebe2648a10e110703be411deea
psteiger <psteiger@gmail.com>
2009-09-18 17:32:19 -0700

    changed from λ to lambda

--- s12/p3.scm
+++ s12/p3.scm
@@ -1,11 +1,12 @@
 #lang scheme
-;; search-lolos : symbol list : boolean
-;; consumes a symbol and a list of list of symbols and returns true if
+
+;; search-lolos : symbol list-of-list : boolean
+;; consumes a symbol and a list of a list of symbols and returns true if
 ;; the symbol is found anywhere in the original list of lists; otherwise, 
 ;; it returns false.
 (define (search-lolos s lst)
   (let ([joint-lists (foldl append '() lst)])
-    (foldl (λ (x bool) 
+    (foldl (lambda (x bool) 
              (or (symbol=? s x) bool)) 
            #f 
            joint-lists)))
